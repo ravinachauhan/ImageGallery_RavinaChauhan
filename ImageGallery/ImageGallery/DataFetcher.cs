08a7ab8214fbca925aa031bf118ac5fb38eec6c1
ravinachauhan <66813151+ravinachauhan@users.noreply.github.com>
2021-08-21 18:25:23 +0530

    Add files via upload

---
+++ ImageGallery/ImageGallery/DataFetcher.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Net;
+using System.Net.Http;
+using System.IO;
+
+namespace ImageGallery
+{
+    class DataFetcher
+    {
+        //This method uses the HttpClient class to fetch the JSON data from the server.
+        async Task<string> GetDatafromService(string searchstring)
+        {
+            string readText = null;
+            try
+            {
+                String url = @"https://imagefetcherapi.azurewebsites.net/api/fetch_images?query=" +
+               searchstring + "&max_count=5";
+                using (HttpClient c = new HttpClient())
+                {
+                    readText = await c.GetStringAsync(url);
+                }
+            }
+            catch
+            {
+                readText = File.ReadAllText(@"Data/sampleData.json");
+            }
+            return readText;
+        }
+        public async Task<List<ImageItem>> GetImageData(string search)
+        {
+            string data = await GetDatafromService(search);
+            //Here, we use the JsonConvert.DeserializeObject() method of Newtonsoft.Json to parse the json data 
+            //into an instance of ImageItem
+            return JsonConvert.DeserializeObject<List<ImageItem>>(data);
+        }
+
+    }
+}
---
+++ ImageGallery/ImageGallery/ImageGallery.cs
@@ -0,0 +1,250 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using C1.Win.C1Tile;
+
+namespace ImageGallery
+{
+    public partial class ImageGallery : Form
+    {
+        DataFetcher datafetch = new DataFetcher();
+        List<ImageItem> imagesList;
+        //Variable to keep a count of the checked tiles.
+        int checkedItems = 0;
+        C1.C1Pdf.C1PdfDocument imagePdfDocument = new C1.C1Pdf.C1PdfDocument();
+        //Controls.
+        private TextBox txtBox = new TextBox();
+        private Button btnAdd = new Button();
+        private ListBox lstBox = new ListBox();
+        private CheckBox chkBox = new CheckBox();
+        private Label lblCount = new Label();
+        public ImageGallery()
+        {
+            InitializeComponent();
+        }
+
+        //Fetch the images using DataFetcher class when search button is clicked.
+        private async void _search_Click(object sender, EventArgs e)
+        {
+            statusStrip1.Visible = true;
+            imagesList = await datafetch.GetImageData(_searchBox.Text);
+            AddTiles(imagesList);
+            statusStrip1.Visible = false;
+        }
+
+        //The add addTiles method will loop through all the images and add it to the tile control.
+        private void AddTiles(List<ImageItem> imageList)
+        {
+            _imageTileControl.Groups[0].Tiles.Clear();
+            foreach (var imageitem in imageList)
+            {
+                Tile tile = new Tile();
+                tile.HorizontalSize = 2;
+                tile.VerticalSize = 2;
+                _imageTileControl.Groups[0].Tiles.Add(tile);
+                Image img = Image.FromStream(new
+                MemoryStream(imageitem.Base64));
+                Template tl = new Template();
+                ImageElement ie = new ImageElement();
+                ie.ImageLayout = ForeImageLayout.Stretch;
+                tl.Elements.Add(ie);
+                tile.Template = tl;
+                tile.Image = img;
+            }
+        }
+        
+        //Increment the counter when tile is checked and displays _exportImage and _savefile boxes.
+        private void _imageTileControl_TileChecked(object sender, C1.Win.C1Tile.TileEventArgs e)
+        {
+            
+            checkedItems++;
+            _exportImage.Visible = true;
+            _saveFile.Visible = true;
+
+        }
+
+        //Decrement the counter
+        private void _imageTileControl_TileUnchecked(object sender, C1.Win.C1Tile.TileEventArgs e)
+        {
+             checkedItems--;
+             _exportImage.Visible = checkedItems > 0;
+        }
+
+        //Converts the file to pdf when export button is clicked.
+        private void _exportImage_Click(object sender, EventArgs e)
+        {
+            List<Image> images = new List<Image>();
+            foreach (Tile tile in _imageTileControl.Groups[0].Tiles)
+            {
+                if (tile.Checked)
+                {
+                    images.Add(tile.Image);
+                }
+            }
+            ConvertToPdf(images);
+            SaveFileDialog saveFile = new SaveFileDialog();
+            saveFile.DefaultExt = "pdf";
+            saveFile.Filter = "PDF files (*.pdf)|*.pdf*";
+
+            if (saveFile.ShowDialog() == DialogResult.OK)
+            {
+
+                imagePdfDocument.Save(saveFile.FileName);
+
+            }
+
+        }
+        private void ConvertToPdf(List<Image> images)
+        {
+            RectangleF rect = imagePdfDocument.PageRectangle;
+            bool firstPage = true;
+            foreach (var selectedimg in images)
+            {
+                if (!firstPage)
+                {
+                    imagePdfDocument.NewPage();
+                }
+                firstPage = false;
+                rect.Inflate(-72, -72);
+                imagePdfDocument.DrawImage(selectedimg, rect);
+            }
+
+        }
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            Rectangle r = _searchBox.Bounds;
+            r.Inflate(3, 3);
+            Pen p = new Pen(Color.LightGray);
+            e.Graphics.DrawRectangle(p, r);
+
+        }
+
+        //Used for drawing a grey border for export to pdf button.
+        private void _exportImage_Paint(object sender, PaintEventArgs e)
+        {
+            Rectangle r = new Rectangle(_exportImage.Location.X, _exportImage.Location.Y, _exportImage.Width, _exportImage.Height);
+            r.X -= 29;
+            r.Y -= 3;
+            r.Width--;
+            r.Height--;
+            Pen p = new Pen(Color.LightGray);
+            e.Graphics.DrawRectangle(p, r);
+            e.Graphics.DrawLine(p, new Point(0, 43), new
+            Point(this.Width, 43));
+        }
+
+        //Used to draw a separator.
+        private void _imageTileControl_Paint(object sender, PaintEventArgs e)
+        {
+            Pen p = new Pen(Color.LightGray);
+            e.Graphics.DrawLine(p, 0, 43, 800, 43);
+
+        }
+
+        //Saves the selected images to local system.
+        private void _saveFile_Click(object sender, EventArgs e)
+        {
+            // Displays a SaveFileDialog so the user can save the Image
+            // assigned to Button2.
+            List<Image> images = new List<Image>();
+            foreach (Tile tile in _imageTileControl.Groups[0].Tiles)
+            {
+                if (tile.Checked)
+                {
+                    images.Add(tile.Image);
+                }
+            }
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.Filter = "JPeg Image|*.jpg|Bitmap Image|*.bmp|Gif Image|*.gif";
+            saveFileDialog1.Title = "Save an Image File";
+            saveFileDialog1.ShowDialog();
+
+            // If the file name is not an empty string open it for saving.
+            if (saveFileDialog1.FileName != "")
+            {
+                // Saves the Image via a FileStream created by the OpenFile method.
+                System.IO.FileStream fs = (System.IO.FileStream)saveFileDialog1.OpenFile();
+                // Saves the Image in the appropriate ImageFormat based upon the
+                // File type selected in the dialog box.
+                // NOTE that the FilterIndex property is one-based.
+                foreach (var selectedimg in images)
+                {
+                    switch (saveFileDialog1.FilterIndex)
+                    {
+                        case 1:
+                            selectedimg.Save(fs,
+                              System.Drawing.Imaging.ImageFormat.Jpeg);
+                            break;
+
+                        case 2:
+                            selectedimg.Save(fs,
+                              System.Drawing.Imaging.ImageFormat.Bmp);
+                            break;
+
+                        case 3:
+                            selectedimg.Save(fs,
+                              System.Drawing.Imaging.ImageFormat.Gif);
+                            break;
+                    }
+
+                    fs.Close();
+                }
+            }
+        }
+
+        //dynamic controls
+        private void ImageGallery_Load(object sender, EventArgs e)
+        {
+            //Set up the form.
+            
+            
+            this.BackColor = Color.White;
+            this.ForeColor = Color.Black;
+            this.Size = new System.Drawing.Size(780, 788);
+            this.Text = "Image Gallery";
+            this.FormBorderStyle = FormBorderStyle.FixedDialog;
+            this.StartPosition = FormStartPosition.CenterScreen;
+  
+            //Format controls. Note: Controls inherit color from parent form.
+            this.btnAdd.BackColor = Color.Black;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.Location = new System.Drawing.Point(90, 25);
+            this.btnAdd.Size = new System.Drawing.Size(50, 25);
+            this.btnAdd.BringToFront();
+
+            this.txtBox.Text = "Text";
+            this.txtBox.Location = new System.Drawing.Point(10, 25);
+            this.txtBox.Size = new System.Drawing.Size(70, 20);
+
+            this.lstBox.Items.Add("One");
+            this.lstBox.Items.Add("Two");
+            this.lstBox.Items.Add("Three");
+            this.lstBox.Items.Add("Four");
+            this.lstBox.Sorted = true;
+            this.lstBox.Location = new System.Drawing.Point(10, 55);
+            this.lstBox.Size = new System.Drawing.Size(130, 95);
+
+            this.chkBox.Text = "Disable";
+            this.chkBox.Location = new System.Drawing.Point(15, 190);
+            this.chkBox.Size = new System.Drawing.Size(110, 30);
+
+            this.lblCount.Text = lstBox.Items.Count.ToString() + " items";
+            this.lblCount.Location = new System.Drawing.Point(55, 160);
+            this.lblCount.Size = new System.Drawing.Size(65, 15);
+
+            //Add controls to the form.
+            this.Controls.Add(btnAdd);
+            this.Controls.Add(txtBox);
+            this.Controls.Add(lstBox);
+            this.Controls.Add(chkBox);
+            this.Controls.Add(lblCount);
+        }
+    }
+}
